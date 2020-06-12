bbd7e1c722c0073452397f8ef9cf30ae8182b79b
Eric Ramos <62032568+kornik32@users.noreply.github.com>
2020-06-12 03:07:53 -0500

    Ampliación de tabla
    Se le añadieron 4 productos a la tabla y se importaron para imprimirla 
    en consola.

--- pruebasql.py
+++ pruebasql.py
@@ -2,6 +2,20 @@ import sqlite3
 
 miConexion = sqlite3.connect("PrimeraBase")
 miCursor = miConexion.cursor()
-miCursor.execute("CREATE TABLE PRODUCTOS\
- (NOMBRE_ARTICULO VARCHAR(50), PRECIO INTEGER, SECCION VARCHAR(20))")
+
+# miCursor.execute("CREATE TABLE PRODUCTOS\
+#  (NOMBRE_ARTICULO VARCHAR(50), PRECIO INTEGER, SECCION VARCHAR(20))")
+# miCursor.execute("INSERT INTO PRODUCTOS VALUES ('BALON', 15, 'DEPORTES')")
+# variosProductos = [("Camiseta", 10, "Deportes"),
+#                    ("Jarrón", 90, "Cerámica"),
+#                    ("Camión", 20, "Juguetería")]
+#
+# miCursor.executemany("INSERT INTO PRODUCTOS VALUES (?,?,?)", variosProductos)
+
+miCursor.execute("SELECT * FROM PRODUCTOS")
+variosProductos = miCursor.fetchall()
+miConexion.commit()
 miConexion.close()
+for producto in variosProductos:
+    print("Nombre:", producto[0], "Precio: ",
+          producto[1], "Seccion: ", producto[2])
