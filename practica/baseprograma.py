101bdd745c41110b3d607fec78254941fc90fb41
Eric Ramos <62032568+kornik32@users.noreply.github.com>
2020-06-14 22:00:42 -0500

    Se cambian nombres a funciones

--- practica/baseprograma.py
+++ practica/baseprograma.py
@@ -1,24 +1,29 @@
 import sqlite3
+from funcionesMenu import tablaCreada
 
 miConexion = sqlite3.connect("Usuario")
 miCursor = miConexion.cursor()
 
 
-def crearTablas():
-    miCursor.execute("""CREATE TABLE DATOS_USUARIOS
-    (ID INTEGER PRIMARY KEY AUTOCOMPLEATE,
-    NOMBRE_USUARIO VARCHAR (50),
-    PASSWORD VARCHAR (50),
-    APELLIDO VARCHAR (20),
-    COMENTARIOS VARCHAR (100))""")
+def crearTabla():
+    try:
+        miCursor.execute("""CREATE TABLE DATOS_USUARIOS (
+        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        NOMBRE_USUARIO VARCHAR (50),
+        PASSWORD VARCHAR (50),
+        APELLIDO VARCHAR (20),
+        COMENTARIOS VARCHAR (100))""")
+    except sqlite3.OperationalError:
+        return tablaCreada()
 
 
-def nuevoDato():
-    pass
+def nuevoDato(nombre, passw, apellido, comentarios):
+    miCursor.execute("""INSERT INTO DATOS_USUARIOS VALUES (
+    NULL, {}, {}, {}, {})""".format(nombre, passw, apellido, comentarios))
 
 
 def editarDato():
-    pass
+    miCursor.execute("")
 
 
 def borrarDato():
@@ -29,4 +34,16 @@ def leerDato():
     pass
 
 
-miConexion.close()
+def cerrarBase():
+    miConexion.close()
+
+
+def probar():
+    miCursor.execute("SELECT * FROM DATOS_USUARIOS")
+    productos = miCursor.fetchall()
+    for producto in productos:
+        print(producto)
+    print("-------------------")
+
+
+probar()
